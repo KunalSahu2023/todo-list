446c5f6e6a0f0605a1208856ccfa0cd4b80abcef
KunalSahu2023 <kunalsahu3131@gmail.com>
2026-09-05 09:40:28 +0530

    feat: add redis template connection

---
+++ src/main/java/com/developer/todolist/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.developer.todolist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+
+        RedisTemplate<String, Object> template= new RedisTemplate<>();
+
+        template.setConnectionFactory(redisConnectionFactory);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
